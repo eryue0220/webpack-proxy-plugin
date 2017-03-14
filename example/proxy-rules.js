029c007a21428361484a581376a4cd3e6ed80e37
= <chenshigang1@letv.com>
2017-03-14 14:48:34 +0800

    bug fixed && update to v1.0.2

--- example/proxy-rules.js
+++ example/proxy-rules.js
@@ -2,13 +2,13 @@
 module.exports = {
   'xxx.xxx.com': {
     // sdk prject name
-    project: 'sdk_name',
+    project: 'report_sdk',
     // the file after compile which can `console.log`
     develop_path:`${__dirname}/dist/develop`,
     // the file after compile which has been compress and drop console
     production_path: `${__dirname}/dist/production`,
     // sdk url path: only support regexp
-    url: /\/sdk\/sdk\.js/,
+    url: /\/sdk\/\w+\.js/,
     // true will read the file where in develop path, instead
     // will read file through production_path
     debugMode: true,
